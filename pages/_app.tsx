7c9115ae4c5692052521b417c4e924a718667656
devakapatel <dev.akash.patel@gmail.com>
2024-10-02 21:40:40 +0530

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,14 @@
 import "../styles/styles.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <script
+        defer
+        src="https://static.cloudflareinsights.com/beacon.min.js"
+        data-cf-beacon='{"token": "d56a42c085dd4918bf504a2f33f140f4"}'
+      ></script>
+    </>
+  );
 }

cb7416c27033bd4e8426d4835f4b1156006c37cb
devakapatel <dev.akash.patel@gmail.com>
2024-12-09 04:50:13 +0530

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,11 +4,6 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-      <script
-        defer
-        src="https://static.cloudflareinsights.com/beacon.min.js"
-        data-cf-beacon='{"token": "d56a42c085dd4918bf504a2f33f140f4"}'
-      ></script>
     </>
   );
 }

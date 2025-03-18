c355b390ee9417737e22515b44c8a948ae7343b3
devakapatel <dev.akash.patel@gmail.com>
2025-03-18 16:59:01 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,8 +30,10 @@ export default async function RootLayout({ children }) {
   return (
     <html lang="en" dir="ltr" suppressHydrationWarning>
       <Head>
-        <link rel="shortcut icon" href="/images/general/icon.svg" />
-        {/* Your additional tags should be passed as `children` of `<Head>` element */}
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏁</text></svg>"
+        />
       </Head>
       <body>
         <Layout

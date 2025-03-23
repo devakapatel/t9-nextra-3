5914ad14609179e738f4db8ae5be6a3d69fd64fe
devakapatel <dev.akash.patel@gmail.com>
2025-03-24 04:54:43 +0530

    update
    system fonts and code fonts

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,33 @@
 import "../styles/styles.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        {/* Preload Inter font */}
+        <link
+          rel="preload"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+          as="style"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+        />
+
+        {/* Preload JetBrains Mono font */}
+        <link
+          rel="preload"
+          href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;600;700&display=swap"
+          as="style"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;600;700&display=swap"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }

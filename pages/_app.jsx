e1a99aaaea81f68df0836cf5a52668879d3c0d21
devakapatel <dev.akash.patel@gmail.com>
2025-03-27 03:15:14 +0530

    update

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,32 +2,5 @@ import "../styles/styles.css";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        {/* Preload Inter font */}
-        <link
-          rel="preload"
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
-          as="style"
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
-        />
-
-        {/* Preload JetBrains Mono font */}
-        <link
-          rel="preload"
-          href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;600;700&display=swap"
-          as="style"
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;600;700&display=swap"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
